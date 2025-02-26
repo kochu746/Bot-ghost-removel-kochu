5cd7c810e7ad6532ac924674d1aec61ae474ba74
kochu746 <gokuzgamer20@gmail.com>
2025-02-26 18:37:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching All Channels And Members", "Watching Trk Official Discord Server"
+const statusMessages = ["Watching SvK CustomZ On Top", "${member.guild.memberCount}"
 const statusTypes = [ 'dnd', 'idle']
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
