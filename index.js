a5023992e9fae70e04ef2e8e9083006c47d50e26
kochu746 <gokuzgamer20@gmail.com>
2025-02-26 18:42:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching SvK CustomZ On Top", "${member.guild.memberCount}"
+const statusMessages = ["Watching SvK CustomZ On Top", "Watching SvK CustomZ On Top"
 const statusTypes = [ 'dnd', 'idle']
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
